b59403c23872d76f892d61fb1aafbb70cea40d78
mad-jules <yuliia.pechera@gmail.com>
2025-11-15 14:44:30 +0200

    chore: implement remote backend api instance

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
 const baseUrl = process.env.NEXT_PUBLIC_API_URL + '/api';
+const baseRemoteUrl = process.env.NEXT_PUBLIC_REMOTE_API_URL + '/api';
 
 export const api = axios.create({
   baseURL: baseUrl,
+  withCredentials: true,
+});
+
+export const remoteApi = axios.create({
+  baseURL: baseRemoteUrl,
   withCredentials: true,
 });
