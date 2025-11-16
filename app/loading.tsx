9e902a6666bda9d71cca49b4b18b63e3d74fb68a
Olexander Motyl <JustSanya1@gmail.com>
2025-11-16 16:46:35 +0100

    add loading background

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,12 +2,14 @@ import css from './loading.module.css';
 
 export default function Loading() {
   return (
-    <div className={css.loadingBackdrop}>
-      <img
-        src="/a cartoon-style chef.webp"
-        alt="Chef Hat"
-        className={css.hat}
-      />
+    <div className={css.loadingBackground}>
+      <div className={css.loadingBackdrop}>
+        <img
+          src="/a cartoon-style chef.webp"
+          alt="Chef Hat"
+          className={css.hat}
+        />
+      </div>
     </div>
   );
 }
