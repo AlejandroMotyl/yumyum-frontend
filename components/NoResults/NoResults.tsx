2f398b2d65be1f9ec66784264f7f1eef049e6a22
Oleksandr Dmytriev <sanderdsukr@gmail.com>
2025-11-20 23:43:01 +0200

    reset all logic

--- components/NoResults/NoResults.tsx
+++ components/NoResults/NoResults.tsx
@@ -1,13 +1,16 @@
+'use client';
+
 import css from './NoResults.module.css';
-import Container from '../Container/Container';
+import { useResetAll } from '@/lib/store/useResetAll';
 
 const NoResults = () => {
+  const resetAll = useResetAll();
   return (
     <div className={css.noResultsWrapper}>
       <h2 className={css.noResultsHeader}>
         We’re sorry! We were not able to find a match.
       </h2>
-      <button className={css.resButton} type="button">
+      <button className={css.resButton} type="button" onClick={resetAll}>
         Reset search and filters
       </button>
     </div>
