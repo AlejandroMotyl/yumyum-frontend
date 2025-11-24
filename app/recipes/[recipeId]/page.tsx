ed0b9f0e866fab0c435b74b9f62460758fc6094e
Oleg Shapoval <olegkazhan@gmail.com>
2025-11-24 17:18:42 +0200

    metadata for recipeId - description will be the first sentence

--- app/recipes/[recipeId]/page.tsx
+++ app/recipes/[recipeId]/page.tsx
@@ -6,33 +6,33 @@ import {
 import { getRecipeById } from '@/lib/api/clientApi';
 import RecipeDetailsClient from './RecipeDetails.client';
 import { Metadata } from 'next';
+import { generateMetadataGlobal } from '@/utils/generateMetadataGlobal';
 
 interface RecipeDetailsProps {
   params: Promise<{ recipeId: string }>;
 }
+//Description will be the first sentence of the recipe description
+const getFirstSentence = (text: string): string => {
+  const dotIndex = text.indexOf('.');
+  return dotIndex > 0 ? text.slice(0, dotIndex + 1) : text;
+};
 
 export const generateMetadata = async ({
   params,
 }: RecipeDetailsProps): Promise<Metadata> => {
   const { recipeId } = await params;
   const recipe = await getRecipeById(recipeId);
-  return {
+  return generateMetadataGlobal({
     title: recipe.title,
-    description: recipe.description.slice(0, 30) + '..',
-    openGraph: {
-      title: recipe.title,
-      description: recipe.description.slice(0, 30) + '..',
-      url: `https://nodejs-hw-zdyd.onrender.com/api/recipes/${recipeId}`,
-      images: [
-        {
-          url: recipe.thumb || '/public/img-default/default-img-tablet.jpg',
-          width: 1200,
-          height: 630,
-          alt: recipe.title,
-        },
-      ],
+    description: getFirstSentence(recipe.description),
+    path: `recipes/${recipeId}`,
+    image: {
+      url: recipe.thumb || '/img-default/default-img-tablet.jpg',
+      width: 1200,
+      height: 630,
+      alt: recipe.title,
     },
-  };
+  });
 };
 
 const RecipeDetailsPage = async ({ params }: RecipeDetailsProps) => {
